a5629d4f314aaaa4ecc5e7dc4ec7f3ccc84c0acd
Mike McPherson <mmcp42@yahoo.com>
2014-02-21 14:27:51 +0000

    added support for BMP180
    defines to tell which sensor to use

--- barometer.ino
+++ barometer.ino
@@ -7,24 +7,46 @@
 //  December 2013
 //==============================================================
 
-//=================================
+//=================================================
 // barometer
 //
 // uses SCL/SDA pins
-//=================================
-//#include <Barometer.h>
-#include <ADAFRUIT_BMP085.h>
+//
+// define BMP085 or BMP180 to select barometer type
+//=================================================
+
+// #define BMP085 1
+#define BMP180 1
+
 #include <Wire.h>
+
+#ifdef BMP085
+#include <ADAFRUIT_BMP085.h>
 Adafruit_BMP085 barometer;
+#endif
+
+#ifdef BMP180
+#include <SFE_BMP180.h>
+SFE_BMP180 pressure;
+#endif
 
 //=====================================================================================
 // routine to get pressure from barometer
 //=====================================================================================
 void barometerGetPressure(void)
 {
+#ifdef BMP085
   float temp;
   long pressure;
+#endif
 
+#ifdef BMP180
+  uint8_t status;
+  double T, P;
+#endif
+
+
+#ifdef BMP085
   // get the temperature
   //====================
   temp = barometer.readTemperature();
@@ -34,6 +56,64 @@ void barometerGetPressure(void)
   //=================
   pressure = barometer.readPressure();
   dataRecord.pressure = pressure / 100;
+#endif
+
+#ifdef BMP180
+  // start the process
+  //==================
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete
+    //=====================================
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T
+    // Function returns 1 if successful, 0 if failure
+    //======================================================
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait)
+      // If request is successful, the number of ms to wait is returned
+      // If request is unsuccessful, 0 is returned
+      //===================================================================================
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete
+        //=====================================
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P
+        // Note also that the function requires the previous temperature measurement (T)
+        // Function returns 1 if successful, 0 if failure
+        //==============================================================================
+        status = pressure.getPressure(P, T);
+        if (status != 0)
+        {}
+        else 
+          DIAGPRINTLN(F("error retrieving pressure measurement"));
+      }
+      else 
+        DIAGPRINTLN(F("error starting pressure measurement"));
+    }
+    else DIAGPRINTLN(F("error retrieving temperature measurement"));
+  }
+  else 
+    DIAGPRINTLN(F("error starting temperature measurement"));
+
+  dataRecord.temperatureP = T * 10;
+  dataRecord.pressure = P;  
+  
+  DIAGPRINT(F("[BMP180] Pressure(mBar): "));
+  DIAGPRINT(P, 1);
+  DIAGPRINT(F("    Temperature(C): "));
+  DIAGPRINTLN(T, 1);
+#endif
 }
 
 //=====================================================================================
@@ -41,8 +121,17 @@ void barometerGetPressure(void)
 //=====================================================================================
 void barometerInit(void)
 {
+#ifdef BMP085
   barometer.begin();
   DIAGPRINT("b");
+#endif
+
+#ifdef BMP180
+  if (pressure.begin())
+    DIAGPRINT("b");
+  else
+    DIAGPRINTLN(F("BMP180 init failed"));
+#endif
 }
 
 //=====================================================================================
